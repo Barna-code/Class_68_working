5c4dd85e523c719a36fd94d8220846e5f94dabcf
Barna-code <68315646+Barna-code@users.noreply.github.com>
2021-04-12 10:51:13 +0530

    Add files via upload

---
+++ screens/TransactionScreen.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class TransactionScreen extends Component {
+
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+
+            >
+
+                <Text>
+
+                    ISSUE/RETURN
+                </Text>
+            </View>
+        )
+    }
+}
